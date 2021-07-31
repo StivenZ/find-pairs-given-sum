10c8001e8670990385bd204f1a2b51c048bef5a2
stivenz <1806@holbertonschool.com>
2021-07-31 17:26:58 -0500

    Updated starting script

--- src/index.js
+++ src/index.js
@@ -1,37 +1,23 @@
 #!/usr/bin/env node
-import Axios from "axios";
-import { findPairs, findPairs2, findPairs3 } from "./algorithms.js";
+import { findPairsOn, findPairsOn2 } from "./algorithms.js";
 import printformat from "./format.js";
+import fetchData from "./client.js";
 
 const sum = parseInt(process.argv[2]);
 const code = parseInt(process.argv[3]);
 const apiLink = "https://mach-eight.uc.r.appspot.com/";
 
-async function fetchData() {
-  try {
-    const response = await Axios.get(apiLink);
-    return response.data["values"];
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-// const data = await fetchData();
-let data = [
-  { first_name: "Alex", h_in: "77", h_meters: "1.96", last_name: "Acker" },
-  { first_name: "Hassan", h_in: "77", h_meters: "1.96", last_name: "Adams" },
-  { first_name: "Arron", h_in: "78", h_meters: "1.97", last_name: "Afflalo" },
-  { first_name: "Maurice", h_in: "79", h_meters: "1.98", last_name: "Ager" },
-  { first_name: "Alexis", h_in: "79", h_meters: "1.98", last_name: "Ajinca" },
-];
+const data = await fetchData(apiLink);
 
 let pairsArray = [];
 if (!code) {
-  pairsArray = findPairs3(sum, data);
+  pairsArray = findPairsOn(sum, data);
 } else if (code === 1) {
-  pairsArray = findPairs(sum, data);
-} else if (code === 2) {
-  pairsArray = findPairs2(sum, data);
+  pairsArray = findPairsOn2(sum, data);
 }
 
-printformat(pairsArray);
+if (pairsArray.length === 0) {
+  console.log(`No pairs were found whose sum added up to ${sum}`);
+} else {
+  printformat(pairsArray);
+}
